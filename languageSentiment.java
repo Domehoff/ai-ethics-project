0c01128d9d0dffcd1a9ec168f5a5654355f3bf0a
Domehoff <domhof@live.com>
2021-04-18 14:16:22 -0400

    Updated languageSentiment.java

--- languageSentiment.java
+++ languageSentiment.java
@@ -1,6 +1,14 @@
 
 import java.io.*;
 import java.util.*;
+
+import edu.stanford.nlp.io.*;
+import edu.stanford.nlp.ling.*;
+import edu.stanford.nlp.pipeline.*;
+import edu.stanford.nlp.trees.*;
+import edu.stanford.nlp.util.*;
+import edu.stanford.nlp.trees.TreeCoreAnnotations.*;
+
 //import packages
 public class languageSentiment{
 	public static void main(String[] args){
@@ -14,7 +22,7 @@ public class languageSentiment{
 		Scanner reader;
 		try {
 			reader = new Scanner(new File("geo_2020-04-21.json"));
-		
+
 		String in = "";
 		long id = -1;
 		String text = "";
@@ -23,12 +31,12 @@ public class languageSentiment{
 		int countryIncrement = 0;
 		boolean flag = true;
 		String temp;
-		
+
 			//CHANGE THE TIME TO GET MORE DATA, PARSING THE WHOLE FILE WILL TAKE A LONG TIME
 		double maxTime = 10000;
-			
+
 		double currentTime = System.currentTimeMillis();
-		
+
 		while(flag = true){
 			in = "  ";
 			while(!in.substring(in.length()-2,in.length()).equals("]}")) {
@@ -45,9 +53,9 @@ public class languageSentiment{
 			if(isTagged(in)) {
 				id = getIDFromLine(in);
 				text = getTextFromID(id);
-				
-				
-				
+
+
+
 				if(textIsEnglish(text)){
 					countryCode = getCountryFromLine(in);
 					if(!countries.containsKey(countryCode)) {
@@ -59,9 +67,9 @@ public class languageSentiment{
 					}
 					countries.put(countryCode, countries.get(countryCode)+1);
 					sentiment = generateSentiment(text);
-					
-					
-										
+
+
+
 					String[] splitText = text.split("\\W");
 					for(int i = 0; i < splitText.length; i++) {
 						if(splitText[i].length()>0) {
@@ -73,7 +81,7 @@ public class languageSentiment{
 					}
 				}
 			}
-			
+
 		}
 		//i will make it output to file
 		System.out.println(Arrays.asList(countries));
@@ -88,15 +96,35 @@ public class languageSentiment{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
+
 	}
+static String cleanTweet(String in) {
+  in = in.trim()
+  // remove unnecessary characters and elements
+  .replaceAll("http.*?[\\S]+", "")
+  .replaceAll("@[\\S]+", "")
+  .replaceAll("#", "")
+  .replaceAll("[\\s]+", " ");
+   return in;
+}
 static long getIDFromLine(String in){
 	return Long.parseLong(in.substring(15, in.indexOf("\",\"created_at")));
 }
 static boolean textIsEnglish(String in) {
-	//FILL IN
-	return true;
+	//load all languages:
+	List<LanguageProfile> languageProfiles = new LanguageProfileReader().readAllBuiltIn();
+	LanguageDetector languageDetector = LanguageDetectorBuilder.create(NgramExtractors.standard())
+        	.withProfiles(languageProfiles)
+        	.build();
+	TextObjectFactory textObjectFactory = CommonTextObjectFactories.forDetectingOnLargeText();
+	TextObject textObject = textObjectFactory.forText(in);
+	Optional<LdLocale> lang = languageDetector.detect(textObject);
+	if(lang.equals("en") || lang.equals("English"))
+	{
+		return true;
+	}
+	return false;
 }
 static String getTextFromID(long id){
 	//FILL IN
@@ -111,7 +139,27 @@ static String getCountryFromLine(String in){
 static int generateSentiment(String text){
 	//FILL IN
 	//1 if positive, 0 if negative
-	return -1;
+	Properties props = new Properties();
+	props.setProperty("annotators", "tokenize, ssplit, pos, parse, sentiment");
+
+	StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+	Annotation annotation = pipeline.process(text);
+
+	for (CoreMap sentence : annotation.get(CoreAnnotations.SentencesAnnotation.class)) {
+		Tree tree = sentence.get(SentimentCoreAnnotations.SentimentAnnotatedTree.class);
+		int temp = RNNCoreAnnotations.getPredictedClass(tree);
+
+		// if the tweet is scored 0 or 1, this is a negatively
+		// marked sentiment
+		if(temp == 0 || temp == 1) {
+			return 0;
+		}
+
+		// else this tweet has been marked positively
+		// (since there are 5 sentiment levels 0->4, the nuetral
+		// score of 0 is counted as positive
+		else  {	return 1; }
+	}
 }
 
 }
